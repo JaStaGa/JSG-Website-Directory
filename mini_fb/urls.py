8d04808481eb4c48b04b679d82913fd77a32188f
jaylinsg <jstarlin@bu.edu>
2025-05-27 12:28:27 -0400

    completed hw3

--- mini_fb/urls.py
+++ mini_fb/urls.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from .views import ShowAllProfilesView, ShowProfilePageView
+
 
 urlpatterns = [
-    # path(r'', views.base, name="base"),
+    path('', ShowAllProfilesView.as_view(), name="show_all_profiles"),
+    path('profile/<int:pk>', ShowProfilePageView.as_view(), name="show_profile")
 ]
--- mini_fb/views.py
+++ mini_fb/views.py
@@ -1,17 +1,19 @@
 import random
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
+from django.views.generic import ListView
+from django.views.generic import DetailView
+from .models import Profile
+
 
 import time
 
 # Create your views here.
-def base(request):
+class ShowAllProfilesView(ListView):
+    model = Profile
+    template_name="mini_fb/show_all_profiles.html"
+    context_object_name="profiles"
 
-    response_text = f'''
-    <html>
-    <h1>Hello World!</h1>
-    The current time is {time.ctime()}.
-    </html>
-    '''
- 
-    return HttpResponse(response_text)
+class ShowProfilePageView(DetailView):
+    model = Profile
+    template_name="mini_fb/show_profile.html"
